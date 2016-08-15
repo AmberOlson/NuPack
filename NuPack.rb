d88cea90acc1eed06e3cdae25cf4cb26a41b8aa2
Amber <amberolson@trentu.ca>
2016-08-15 13:56:10 -0400

    material markup calculation refactoring

--- NuPack.rb
+++ NuPack.rb
@@ -2,22 +2,19 @@ class NuPack
 
     def calculate(basePrice, person, material)
 
+        materialHash = {"Pharmaceutical" => 0.075, "Food" => 0.13, "Electronic" => 0.02, "Other" => 0}
+
         # caculates a flat markup on all jobs of 5%
         basePricePlusFLat = basePrice + (basePrice * 0.05)
 
         # caculates a markup of 1.2% for each person on the job
         personMarkup = (basePricePlusFLat * 0.012) * person
 
-        case material
-            when "Pharmaceutical"
-                materialMarkup = (basePricePlusFLat * 0.075)
-            when "Food"
-                materialMarkup = (basePricePlusFLat * 0.13)
-            when "Electronic"
-                materialMarkup = (basePricePlusFLat * 0.02)
-            else
-                materialMarkup = 0
-        end
+
+        # caculates a markup for material used on the job
+        materialMarkupPercent = materialHash[material]
+        materialMarkup = basePricePlusFLat * materialMarkupPercent
+
         #total estimate for the project
         estimate = basePricePlusFLat + personMarkup + materialMarkup
         total = (estimate).round(2)
