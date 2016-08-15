84fa61595f673f51f8e6eac66381c51c53efd211
Amber <amberolson@trentu.ca>
2016-08-15 12:19:16 -0400

    refactoring

--- NuPack.rb
+++ NuPack.rb
@@ -1,10 +1,14 @@
 class NuPack
 
     def calculate(basePrice, person)
+
+        # caculates a flat markup on all jobs of 5%
         basePricePlusFLat = basePrice + (basePrice * 0.05)
 
+        # caculates a markup of 1.2% for each person on the job
         personMarkUp = (basePricePlusFLat * 0.012) * person
 
+        #total estimate for the project
         estimate = basePricePlusFLat + personMarkUp
 
     end
--- NuPackSpec.rb
+++ NuPackSpec.rb
@@ -1,15 +1,18 @@
 require_relative "NuPack"
 
 describe "NuPack" do
+
+    before do
+        @sut = NuPack.new()
+    end
+
     it "calculates the correct base mark up 5% and zero people" do
-        sut = NuPack.new()
-        results = sut.calculate(100, 0)
+        results = @sut.calculate(100, 0)
         expect(results).to   eq(105)
     end
 
     it "calculates the correct mark up for 1 person working on the job" do
-        sut = NuPack.new()
-        results = sut.calculate(100, 1)
+        results = @sut.calculate(100, 1)
         expect(results).to   eq(106.26)
     end
 end
